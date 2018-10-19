72a4203cd128a7af16d9aa181c819f8ba7fc86ed
lovecodestudent <243968772@qq.com>
2018-10-19 09:34:28 +0800

    Add files via upload

---
+++ 1.0.1.cpp
@@ -0,0 +1,21 @@
+/**************************
+*文件名：1.0.1.cpp
+*
+*文件描述：输出hello world
+*
+*创建人：凡定坤
+*
+*时间：2018-7-12
+*
+*版本号：1.0
+*
+*修改记录：
+****************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*输出hello world*/
+    cout << "Hello World" << endl;
+    return 0;
+}
